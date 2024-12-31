fb127ca8f46cc3334b15a97c7265487f9726c364
zheng.boc <zheng.boc@northeastern.edu>
2024-12-31 18:00:30 -0500

    Spring Boot SAML example

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -12,6 +12,11 @@ public class HomeController {
     @RequestMapping("/")
     public String home(@AuthenticationPrincipal Saml2AuthenticatedPrincipal principal, Model model) {
         model.addAttribute("name", principal.getName());
+        String email = principal.getFirstAttribute("email");
+        // if email is not preset, try Auth0 attribute name
+        if (email == null) {
+            email = principal.getFirstAttribute("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress");
+        }
         model.addAttribute("emailAddress", principal.getFirstAttribute("email"));
         model.addAttribute("userAttributes", principal.getAttributes());
         return "home";
